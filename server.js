2c761c77765f0c7ef728f8a214f6738dd9e54de9
lance <dvlance112208@gmail.com>
2026-07-19 12:04:04 +0800

    fix mongo connection

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ app.use(cors());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 
+// DEBUG (check if ENV is working)
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/authdb")
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.log("❌ DB Error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.log("❌ DB Error:", err));
 
 // Port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
